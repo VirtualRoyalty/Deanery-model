090a8af1f08d507303f62c53a189962379bbe6cf
Vadik Alperovich <vadikalperovich@mail.ru>
2019-02-06 02:51:59 +0400

    hellyeah

---
+++ Deanery/src/main.cpp
@@ -0,0 +1,23 @@
+ 
+#include "lab5.h"
+
+#include <fstream>
+
+using namespace std;
+
+
+
+int main()
+{
+	
+	Deanery IMIKN("students.txt");
+	IMIKN.printStatics();
+	string name = "Perkins Steven";
+	string title = "PI";
+	IMIKN.transfer(name, IMIKN.searchGroup(title));
+	IMIKN.checkProgress(2.5);
+	IMIKN.reelection();
+	IMIKN.updateData("studentsUpdate.txt");
+	IMIKN.printStatics();
+	system("pause");
+}
